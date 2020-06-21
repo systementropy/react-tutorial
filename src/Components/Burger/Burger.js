4bcc6af9b7a308798d4e175ca6a2863d96a6b67c
systementropy <vatsa.saket@gmail.com>
2020-06-21 15:21:31 +0530

    Burger

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+const burger = (props) => {
+	return(
+		<div>
+			<BurgerIngredient></BurgerIngredient>
+		</div>
+		
+	)
+}
+export default burger;
