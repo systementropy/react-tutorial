41c0a4b99b56824b68243e9fa9c23dc76063ad4c
systementropy <vatsa.saket@gmail.com>
2020-08-17 16:02:13 +0530

    Navigation Items

---
+++ src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './NavigationItems.module.css'
+import NavigationItem from './Item/NavigationItem'
+const NavigationItems = () => (
+	<ul className={classes.NavigationItems}>
+		<NavigationItem link='/' active>Burger Builder</NavigationItem>
+		<NavigationItem link='/'>Checkout</NavigationItem>
+	</ul>
+);
+export default NavigationItems;
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems';
 const toolBar = (props) => (
 	<header className={classes.Toolbar}>
 		<div>Menu</div>
 		<Logo />
-		<nav>
-			...
-		</nav>
+		<NavigationItems />
 		{/* <button></button> */}
 	</header>
 );
